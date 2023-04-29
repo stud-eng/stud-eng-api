ce38a3ee1906c9428726b4edac242d8de7b805e1
Kantaro0829 <eshangwan@gmail.com>
2023-04-30 03:01:49 +0900

    #4 新規インターフェイス追加

---
+++ internal/usecase/interface.go
@@ -0,0 +1,16 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/stud-eng/stud-eng-api/internal/entity"
+)
+
+type (
+	DBRepository interface {
+		GetTest(context context.Context, id uint32) (*entity.Test, error)
+		InsertTest(context context.Context, test *entity.Test) (*entity.Test, error)
+		UpdateTest(context context.Context, test *entity.Test) (*entity.Test, error)
+		DeleteTest(context context.Context, id uint32) error
+	}
+)
